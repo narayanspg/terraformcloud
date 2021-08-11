19a1247ef50fc25cc4943dd085adefc56f3077f1
narayanspg <31915260+narayanspg@users.noreply.github.com>
2021-08-11 11:29:03 +0530

    Create ec2test.tf

---
+++ ec2test.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "nginxserver" {
+  ami           = "ami-0443305dabd4be2bc"
+  instance_type = "t2.micro"
+  key_name      = "mykey"
+
+  tags = {
+    Name = "orange1"
+  }
+
+}
